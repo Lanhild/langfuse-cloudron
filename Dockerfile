075dcb0aaf35dd7d4aa81f72872bc83454144dd3
Lanhild <joliveau.loan@gmail.com>
2024-08-23 20:36:24 -0400

    Update migration application

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,25 @@
 ARG VERSION=2
 
-FROM cloudron/base:4.2.0@sha256:46da2fffb36353ef714f97ae8e962bd2c212ca091108d768ba473078319a47f4 as base
+FROM cloudron/base:4.2.0@sha256:46da2fffb36353ef714f97ae8e962bd2c212ca091108d768ba473078319a47f4 AS base
 
-FROM langfuse/langfuse:${VERSION} as langfuse
+FROM langfuse/langfuse:${VERSION} AS langfuse
 
 FROM base
 
 ENV NODE_ENV production
 WORKDIR /app/code
 
-RUN npm install -g --no-package-lock --no-save prisma
+ARG NODE_VERSION=20.15.1
+RUN mkdir -p /usr/local/node-${NODE_VERSION} && curl -L https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.gz | tar zxf - --strip-components 1 -C /usr/local/node-${NODE_VERSION}
+ENV PATH /usr/local/node-${NODE_VERSION}/bin:$PATH
+
+RUN npm --global install pnpm
+
+RUN pnpm install prisma prisma-erd-generator prisma-kysely
 
 COPY --from=langfuse ./app ./langfuse
+
+RUN npx prisma generate --schema=/app/code/langfuse/packages/shared/prisma/schema.prisma
 
 COPY env.sh.template start.sh /app/pkg/
 
--- start.sh
+++ start.sh
@@ -15,9 +15,15 @@ if [[ ! -f /app/data/.nextauth_secret ]]; then
   openssl rand -base64 32 > /app/data/.nextauth_secret
 fi
 
+if [[ ! -f /app/data/.encryption_key ]]; then
+  echo "=> Generating ENCRYPTION_KEY"
+  openssl rand -hex 32 > /app/data/.encryption_key
+fi
+
 echo "=> Loading configuration"
 export NEXTAUTH_SECRET="$(cat /app/data/.nextauth_secret)"
 export SALT="$(cat /app/data/.salt)"
+export ENCRYPTION_KEY="$(cat /app/data/.encryption_key)"
 export DATABASE_URL="$CLOUDRON_POSTGRESQL_URL"
 export NEXTAUTH_URL="$CLOUDRON_APP_ORIGIN"
 
@@ -30,8 +36,8 @@ echo "=> Setting permissions"
 chown -R cloudron:cloudron /app/data /run/*.npm
 
 echo "=> Applying migrations"
-prisma db execute --url "$DATABASE_URL" --file "/app/code/langfuse/packages/shared/scripts/cleanup.sql"
-DIRECT_URL="$DATABASE_URL" prisma migrate deploy --schema=/app/code/langfuse/packages/shared/prisma/schema.prisma
+/app/code/node_modules/.bin/prisma db execute --url "$DATABASE_URL" --file "/app/code/langfuse/packages/shared/scripts/cleanup.sql"
+DIRECT_URL="$DATABASE_URL" /app/code/node_modules/.bin/prisma migrate deploy --schema=/app/code/langfuse/packages/shared/prisma/schema.prisma
 
 echo "=> Starting Langfuse"
 exec gosu cloudron:cloudron node /app/code/langfuse/web/server.js
